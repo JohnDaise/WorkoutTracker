36efb0c3d258e9258b213cee74202fe8c1571074
JohnDaise <jdaise@gmail.com>
2020-05-27 08:43:20 -0400

    heroku mongodb setup

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+//"mongodb://localhost/mongoHeadlines"
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
